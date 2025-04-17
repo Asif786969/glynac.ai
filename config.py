b1b22c920a58e0ca3e23e20bc277bbf9872264f0
King <king@debian12.local>
2025-04-17 13:34:15 +0530

    final push

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://asif:asif@localhost:5432/employee_db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
